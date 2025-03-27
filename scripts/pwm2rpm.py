80e47293dfca1a8e9cc5ee99c2fc51e633ef04ff
Wolfgang Hoenig <hoenig@tu-berlin.de>
2025-03-27 17:57:39 +0100

    prm2rpm fixes

--- scripts/pwm2rpm.py
+++ scripts/pwm2rpm.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     # thrust[g],pwm,vbat[V],rpm1,rpm2,rpm3,rpm4
 
-    thrust = data[:,0] / 4  # g, per motor
+    thrust = (data[:,0] / 1000.0) / 4 * 9.81  # N, per motor
     pwm = data[:,1]         # PWM value
     vbat = data[:,2]        # V, battery voltage, 
 
@@ -35,29 +35,31 @@ if __name__ == '__main__':
 
     a = cp.Variable()
     b = cp.Variable()
-    # pwm = a + b * thrust
-    cost = cp.sum_squares(a + b * thrust - pwm_normalized)
+    c = cp.Variable()
+    # pwm = a + b * thrust + c * vbat * thrust
+    cost = cp.sum_squares(a + b * thrust + c * cp.multiply(vbat, thrust) - pwm_normalized)
     prob = cp.Problem(cp.Minimize(cost), [])
     prob.solve()
-    print("rpm2pwm: {}".format(a.value))
-    print("rpm2pwm: {}".format(b.value))
+    print("rpm2pwm: a {}".format(a.value))
+    print("rpm2pwm: b {}".format(b.value))
+    print("rpm2pwm: c {}".format(c.value))
 
-    eval = a.value + b.value * thrust
+    eval = a.value + b.value * thrust + c.value * vbat * thrust
 
     # plotting
 
     fig, ax = plt.subplots(2)
     ax[0].plot(thrust)
     ax[0].set_xlabel('Time')
-    ax[0].set_ylabel('thrust [g]')
+    ax[0].set_ylabel('thrust [N]')
     ax[0].legend()
     ax[0].grid(True)
 
     ax[1].scatter(thrust, pwm_normalized, label='data')
-    ax[1].plot(thrust, eval, label='fit')
+    ax[1].scatter(thrust, eval, label='fit')
 
-    ax[1].set_xlabel('thrust')
-    ax[1].set_ylabel('pwm')
+    ax[1].set_xlabel('thrust [N]')
+    ax[1].set_ylabel('pwm [0..1]')
     ax[1].legend()
     ax[1].grid(True)
 
